8390abeba19f33041b9251e6eab720a1a0671948
yuzhongjun <124756828@qq.com>
2022-12-18 19:36:24 +0800

    增加专题5的代码

---
+++ 专题5_蓝牙HCI项目介绍/bt_init/bt_init_006/uart.c
@@ -0,0 +1,73 @@
+#include "uart.h"
+#include "btsnoop.h"
+
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+int uart_fd = -1;
+struct termios toptions;
+
+
+void uart_bt_open(void)
+{
+    uart_fd = open("/dev/ttyUSB0",O_RDWR | O_NOCTTY);
+    if(uart_fd < 0)
+    {
+        printf("can not open uart tty port\r\n");
+        return ;
+    }
+
+	if(tcgetattr(uart_fd, &toptions) <0)
+	{
+		printf("can not get terminal attributes\r\n");
+		return;
+	}
+
+	cfmakeraw(&toptions);
+
+	toptions.c_cflag |= CRTSCTS | CREAD | CLOCAL;
+
+	cfsetispeed(&toptions,B921600);
+	cfsetospeed(&toptions,B921600);
+
+	if(tcsetattr(uart_fd,TCSANOW, &toptions) <0)
+	{
+		printf("can not get terminal attributes\r\n");
+		return;
+	}
+	
+
+
+}
+
+void uart_bt_close(void)
+{
+    if(uart_fd > 0)
+        close(uart_fd);
+
+        uart_fd = -1;
+
+}
+
+int uart_bt_send(uint8_t *data, uint16_t len)
+{
+	printf("-------------------\r\n");
+	for(int index = 0; index < len;index++)
+		printf("0x%02x ",data[index]);
+	printf("\r\n-------------------\r\n");
+
+	btsnoop_write(data[0],0,data,len);
+    return write(uart_fd,data,len);
+}
+
+int uart_bt_read(uint8_t *data, uint16_t len)
+{
+    return read(uart_fd,data,len);
+}
+
